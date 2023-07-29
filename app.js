c4061dffb5555f888deed75a6fa2b5065e7e2efb
Akhilesh <makmishra.99@gmail.com>
2023-07-30 00:03:26 +0530

    build-react-app

--- app.js
+++ app.js
@@ -1,8 +1,99 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World with React!" //children
-); // its return react element which is basically object
+/* Bundler in react is basically a tool which allows a developers their code into a single file or bundle which will be
+   elligible to ship it to production 
+   there are total 3 types of bundler , webpack , parcel , beet
+
+   Webpack in react is a JavaScript module bundler that is commonly used with React to bundle and manage dependencies.
+   It takes all of the individual JavaScript files and other assets in a project, such as images and CSS,
+   and combines them into a single bundle that can be loaded by the browser.
+*/
+/* react.createElement - its return react element which is basically object.
+   react element is kind of DOM element but its not a html elements, its basically a object and that object will be render by render
+   method
+*/
+
+// render method takes a object and convert into h1 tag and put it into DOM
+
+/* JSX stands for javascript syntax which is kind of html-link syntax , which actually helps to create the htmls elements otherwise
+   using react it would ve very combursome experience.
+
+   this JSX is totally different from react . it is used only for creating react element in the end and this where babel comes.
+   babel is  js library which is used transpiled jsx syntax to react code so that render method will easily able to render this.
+
+   any attribute inside jsx syntax should be written in camelCase form.
+ */
+
+//Component Composition -  using component inside component
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { resData } from "./constants/restaurantData";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://tse2.mm.bing.net/th?id=OIP.sADwBIBpPMoGuF0ipvdo0AHaHa&pid=Api&P=0&h=180"
+        ></img>
+        <h3>Akhilesh Da Dhaba</h3>
+      </div>
+      <div className="nav-container">
+        <ul className="nav-links">
+          <li> Home</li>
+          <li> About</li>
+          <li> Contact Us</li>
+          <li> Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantContainer = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, avgRating, cuisines } = resData?.info;
+  return (
+    <div className="res-cards">
+      <img
+        className="krishna-prasad"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <div className="food-name">
+        <h4 style={{ fontSize: "0.8rem" }}>Krishna Prasad</h4>
+        <span className="rating-badge">{avgRating} star</span>
+      </div>
+      <div>
+        <p className="food-cuisene">{cuisines.join(", ")}</p>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      {/* <div className="search">Search</div> */}
+      <div className="res-container">
+        {resData.map((data) => {
+          return <RestaurantContainer key={data?.info?.id} resData={data} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // takes a object and convert into h1 tag and put it into DOM
+root.render(<AppLayout />);
---
+++ constants/restaurantData.js
@@ -0,0 +1,633 @@
+export const resData = [
+  {
+    info: {
+      id: "333396",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Sector E",
+      areaName: "Aliganj",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "333396",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2800,
+      },
+      parentId: "166",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 4.4,
+        serviceability: "SERVICEABLE",
+        slaString: "29 mins",
+        lastMileTravelString: "4.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-30 03:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-sector-e-aliganj-lucknow-333396",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "322637",
+      name: "LunchBox - Meals and Thalis",
+      cloudinaryImageId: "src4ymvoqbmbacgpqotk",
+      locality: "Sitapur Road",
+      areaName: "Aliganj",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Biryani",
+        "North Indian",
+        "Punjabi",
+        "Healthy Food",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4,
+      feeDetails: {
+        restaurantId: "322637",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4000,
+      },
+      parentId: "4925",
+      avgRatingString: "4.0",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 5.6,
+        serviceability: "SERVICEABLE",
+        slaString: "32 mins",
+        lastMileTravelString: "5.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-30 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/lunchbox-meals-and-thalis-sitapur-road-aliganj-lucknow-322637",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "61060",
+      name: "Chahat Biryani",
+      cloudinaryImageId: "c46hqxsycegafkxnsi9f",
+      locality: "Aliganj",
+      areaName: "Aliganj",
+      costForTwo: "₹300 for two",
+      cuisines: ["Mughlai", "Indian", "Chinese"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "61060",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4000,
+      },
+      parentId: "56864",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-29 23:40:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chahat-biryani-aliganj-lucknow-61060",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "322634",
+      name: "Behrouz Biryani",
+      cloudinaryImageId: "49c27c020e946f8f4fc13fdc9038525c",
+      locality: "Sitapur Road",
+      areaName: "Aliganj",
+      costForTwo: "₹500 for two",
+      cuisines: [
+        "Biryani",
+        "Mughlai",
+        "Lucknowi",
+        "Hyderabadi",
+        "Kebabs",
+        "North Indian",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "322634",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4000,
+      },
+      parentId: "1803",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 5.6,
+        serviceability: "SERVICEABLE",
+        slaString: "33 mins",
+        lastMileTravelString: "5.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-30 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/behrouz-biryani-sitapur-road-aliganj-lucknow-322634",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "66673",
+      name: "Mr. Brown- DANBRO",
+      cloudinaryImageId: "2c469b5d88f84b7495b9825d06e4bb9b",
+      locality: "Aliganj",
+      areaName: "Aliganj",
+      costForTwo: "₹150 for two",
+      cuisines: [
+        "Bakery",
+        "Italian",
+        "Snacks",
+        "Healthy Food",
+        "Pizzas",
+        "Beverages",
+      ],
+      avgRating: 4.3,
+      feeDetails: {
+        restaurantId: "66673",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4000,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4000,
+      },
+      parentId: "2262",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "30 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-30 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mr-brown-danbro-aliganj-lucknow-66673",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "673472",
+      name: "Rominus Pizza & Burger",
+      cloudinaryImageId: "9ec9ffd900c24ef751e2f34ba3d2fd4b",
+      areaName: "Vikas Nagar",
+      costForTwo: "₹1000 for two",
+      cuisines: ["Pizzas", "Burgers", "Snacks", "Pastas"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "673472",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4500,
+      },
+      parentId: "8387",
+      avgRatingString: "4.2",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 6.6,
+        serviceability: "SERVICEABLE",
+        slaString: "36 mins",
+        lastMileTravelString: "6.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-30 03:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/rominus-pizza-and-burger-vikas-nagar-lucknow-673472",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "73286",
+      name: "Aryan Family's Delight",
+      cloudinaryImageId: "4359e7e08cc028ebb1964bc0aa4ddf59",
+      locality: "Sector 2",
+      areaName: "Khurram Nagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian", "South Indian", "Chinese", "Italian"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "73286",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4500,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4500,
+      },
+      parentId: "6001",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 6.5,
+        serviceability: "SERVICEABLE",
+        slaString: "33 mins",
+        lastMileTravelString: "6.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-07-29 23:45:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹159",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/aryan-familys-delight-sector-2-khurram-nagar-lucknow-73286",
+      type: "WEBLINK",
+    },
+  },
+];
